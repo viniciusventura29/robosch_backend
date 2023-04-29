82d0f3e23e3e54981fc18f04dfcc0d85fc71c14c
viniciusventura29 <viniciusventura29@gmail.com>
2023-04-28 21:10:45 -0300

    new cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cors = require('cors')
 const bodyParser = require("body-parser");
 const { executablePath } = require('puppeteer');
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors({
+  origin:["http://localhost:5173","http://localhost:5173/"]
+}))
 
 require("./routes/df-routes")(app);
 
