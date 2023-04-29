f6077793691dc22b2c12336677cc62832f691290
viniciusventura29 <viniciusventura29@gmail.com>
2023-04-28 22:08:19 -0300

    server and wppconnect server splited

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors')
 const bodyParser = require("body-parser");
-const { executablePath } = require('puppeteer');
+const { run } = require("./wppConnect");
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,43 +13,7 @@ app.use(cors({
 
 require("./routes/df-routes")(app);
 
-const wppconnect = require("@wppconnect-team/wppconnect");
-const { default: axios } = require("axios");
-
-wppconnect
-  .create({
-    session: "sessionName", //Pass the name of the client you want to start the bot
-    catchQR: (asciiQR) => {
-      // console.log("Terminal qrcode: ", asciiQR);
-    },
-    statusFind: (statusSession) => {
-      console.log("Status Session: ", statusSession);
-    },
-    puppeteerOptions:{
-      args:['--no-sandbox','--disable-setuid-sandbox'],
-      executablePath: executablePath()
-    },
-    
-  })
-  .then((client) => start(client))
-  .catch((error) => console.log(error));
-
-function start(client) {
-  client.onMessage((message) => {
-    axios.post("http://localhost:3000/",{
-      text: message.body,
-      userId: message.sender.id
-    }).then((res)=>{
-      console.log(res.data)
-      client
-      .sendText(message.sender.id, res.data)
-      .catch((erro) => {
-        console.error('Error when sending: ', erro);
-      });
-     
-    })
-  });
-}
+require("./wppConnect")(run)
 
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
