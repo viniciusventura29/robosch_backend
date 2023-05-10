2c17717862c2b54afb8c1aa24a02a46d384b1ab3
viniciusventura29 <viniciusventura29@gmail.com>
2023-05-10 13:57:55 -0300

    deleting wppConnect call

--- src/server.js
+++ src/server.js
@@ -8,12 +8,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin:["http://localhost:5173","http://localhost:5173/","*","https://chat-eight-omega.vercel.app/","https://chat-eight-omega.vercel.app"]
+  origin:["http://localhost:5173","http://localhost:5173/","*","https://chat-eight-omega.vercel.app/","https://chat-eight-omega.vercel.app","https://chat-robosch.vercel.app"]
 }))
 
 require("./routes/df-routes")(app);
-
-require("./wppConnect")(run)
 
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
